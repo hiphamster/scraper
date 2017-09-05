8a17fca75b3b687b1a7f6c6b7e1088e2b81d8560
ayelluas@gmail.com <ayelluas@gmail.com>
2017-09-05 00:30:24 -0700

    Initial check-in

---
+++ README.md
@@ -0,0 +1,7 @@
+## A yelp scraping poc
+
+### Install:
+* in virtualenv: pip install -r requirements.txt
+
+### Run:
+* scrapy crawl yelper -o out.json 
---
+++ scraper/spiders/yelp_spider.py
@@ -0,0 +1,36 @@
+import scrapy
+from lxml import html
+
+
+class YelpSpider(scrapy.Spider):
+    name = 'yelper'
+
+    custom_settings = {
+        'USER_AGENT': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) '
+                       'AppleWebKit/537.36 (KHTML, like Gecko) '
+                       'Chrome/60.0.3112.101 Safari/537.36')
+    }
+
+    def start_requests(self):
+        urls = [
+            'https://www.yelp.com/search?find_desc=coffee&find_loc=Berkeley%2C+CA&ns=1'
+        ]
+        for url in urls:
+            req = scrapy.Request(url=url, callback=self.parse)
+            yield req
+
+    def parse(self, response):
+        html_tree = html.fromstring(str(response.body, 'utf-8'))
+
+        for listing in html_tree.findall('.//div[@class="biz-listing-large"]'):
+            name = listing.find(
+                './/a[@data-analytics-label="biz-name"]/span').text.strip()
+            phone = listing.find('.//span[@class="biz-phone"]').text.strip()
+
+            yield {'name': name, 'phone': phone}
+
+        next_page = html_tree.find(
+            './/a[@class="u-decoration-none next pagination-links_anchor"]')
+        if (next_page):
+            next_page = response.urljoin(next_page.get('href'))
+            yield scrapy.Request(url=next_page, callback=self.parse)
